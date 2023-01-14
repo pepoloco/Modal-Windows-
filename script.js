5b315cec2b5c27d85dbfeda961de45d2d6932a94
Pepo Banchev <banchevpepo@gmail.com>
2023-01-14 20:27:43 +0200

    no message

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnClose = document.querySelector('.close-modal');
-const btnOpen = document.querySelectorAll('.show-modal');
+const closeBtn = document.querySelector('.close-modal');
+const openBtn = document.querySelectorAll('.show-modal');
+const overplay = document.querySelector('.overplay');
 
 const openModal = function () {
     modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-};
-
+    overplay.classList.remove('hidden');
+}
 const closeModal = function () {
     modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+    overplay.classList.add('hidden');
 }
 
-for (let i = 0; i < btnOpen.length; i++) {
-    btnOpen[i].addEventListener('click', openModal);
+for (let i = 0; i < openBtn.length; i++) {
+    openBtn[i].addEventListener('click', openModal);
 }
-btnClose.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
 
+closeBtn.addEventListener('click', closeBtn);
+overplay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
-    // console.log(e.key);
+
 
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
